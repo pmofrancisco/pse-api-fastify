29c0cf92d97d6f584cbfc613ba3e56ce76e08af4
Paul Michael Francisco <pmofrancisco@yahoo.com>
2021-06-06 12:40:30 +0800

    Select all DailyQuote columns

--- src/routes/daily-quotes/get-daily-quotes.ts
+++ src/routes/daily-quotes/get-daily-quotes.ts
@@ -5,7 +5,7 @@ import getDateRange from '../../utils/get-date-range';
 export default async (fastify: IFastifyInstance) => {
   const { startDate, endDate } = await getDateRange(fastify, 200);
   return await fastify.db?.models['DailyQuote'].findAll({
-    attributes: ['StockCode', 'ClosePrice'],
+    attributes: ['StockCode', 'QuoteDate', 'OpenPrice', 'HighPrice', 'LowPrice', 'ClosePrice', 'Volume', 'Value'],
     where: {
       QuoteDate: {
         [Op.between]: [startDate, endDate],
